0b1f8d9c6ab2bdabcf3150d13c356bf62e1eddef
Peter Jenkins <pjenkins@mparticle.com>
2020-08-27 16:26:52 -0400

    Add string validation

--- smartype-generator/src/main/kotlin/com/mparticle/smartype/generator/StringHelpers.kt
+++ smartype-generator/src/main/kotlin/com/mparticle/smartype/generator/StringHelpers.kt
@@ -2,11 +2,11 @@ package com.mparticle.smartype.generator
 
 class StringHelpers {
     companion object {
-        fun sanitize(key: String): String {
-            val sanitizedName = upperFirst(stripChars(key))
+        fun sanitize(key: String): String? {
+            val sanitizedName = stripChars(key)?.let { upperFirst(it) }
             var uppercaseName = ""
-            val words = sanitizedName.split(" ", "_", "-")
-            if (words.count() == 0) {
+            val words = sanitizedName?.split(" ", "_", "-")
+            if (words == null || words.count() == 0) {
                 return sanitizedName
             }
             var isFirst = true
@@ -22,19 +22,28 @@ class StringHelpers {
             return uppercaseName
         }
 
-        private fun upperFirst(key: String): String {
+        private fun upperFirst(key: String?): String? {
+            if (key == null || key.isEmpty()) {
+                return null
+            }
             val firstLetter = key[0].toUpperCase()
             val remainingLetters = key.subSequence(1, key.length)
             return "${firstLetter}${remainingLetters}"
         }
 
-        fun lowerFirst(key: String): String {
+        fun lowerFirst(key: String?): String? {
+            if (key == null || key.isEmpty()) {
+                return null
+            }
             val firstLetter = key[0].toLowerCase()
             val remainingLetters = key.subSequence(1, key.length)
             return "${firstLetter}${remainingLetters}"
         }
 
-        private fun stripChars(key: String): String {
+        private fun stripChars(key: String?): String? {
+            if (key == null || key.isEmpty()) {
+                return null
+            }
             return key.replace(Regex("[^a-zA-Z0-9 _-]"), "")
         }
     }
