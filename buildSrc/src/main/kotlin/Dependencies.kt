ad95576527bf1ab46eec8abc55c59104ea538c4f
bstalnaker <bstalnaker@mparticle.com>
2022-03-09 10:11:11 -0500

    fix: fix mutliplatform versioning

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,13 +1,13 @@
 object versions {
-    const val kotlin = "1.4.31"
+    const val kotlin = "1.6.10"
     const val dokka = "1.5.0"
     const val spotless = "3.27.0"
-    const val serialization = "1.1.0"
+    const val serialization = "1.6.10"
 }
 
 object deps {
     object android {
-        const val gradlePlugin = "com.android.tools.build:gradle:4.0.2"
+        const val gradlePlugin = "com.android.tools.build:gradle:7.1.0"
     }
     object mparticle {
         const val androidSdk = "com.mparticle:android-core:5.16.1"
