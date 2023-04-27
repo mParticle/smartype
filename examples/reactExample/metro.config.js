ea165b01b8786eefca9b68334d68fae31e32f7e7
Sam Dozor <sdozor@mparticle.com>
2023-04-27 12:17:09 -0400

    Add React Native sample app

---
+++ examples/reactExample/metro.config.js
@@ -0,0 +1,22 @@
+/**
+ * Metro configuration for React Native
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+const exclusionList = require('metro-config/src/defaults/exclusionList');
+
+module.exports = {
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: true,
+      },
+    }),
+  },
+  resolver: {
+      blockList: exclusionList([/\.smartype\/.*/])
+    },
+};
